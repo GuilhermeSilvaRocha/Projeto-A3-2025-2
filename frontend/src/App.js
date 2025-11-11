e01c4b3b138a8ffcacefc27ef0702d590ae54724
Pedro M Ferrari <pedromferrari@gmail.com>
2025-11-10 23:37:28 -0300

    frontend initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import DenunciaForm from "./components/DenunciaForm";
+
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/denunciar" element={<DenunciaForm />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
